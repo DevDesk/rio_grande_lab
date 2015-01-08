e6f78316b6c8991a0e5b424756ab74ee32e1c47e
Lenny Urbanowski <lenny@itslennysfault.com>
2015-01-07 17:38:05 -0800

    added book spec/model

--- lib/Book.rb
+++ lib/Book.rb
@@ -1,7 +1,11 @@
 require_relative 'Item.rb'
 class Book < Item
 
+    attr_accessor :author, :pages
+
     def initialize name, price
         super(name,price)
+        @pages=0
+        @author=""
     end
 end
